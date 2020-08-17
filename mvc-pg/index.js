3e74609170f587cb35a44dba14e5b7ec81844024
LeeUmpierre <umpierre2507@hotmail.com>
2020-08-17 19:01:09 -0300

    Aula 11/08/20

--- mvc-pg/index.js
+++ mvc-pg/index.js
@@ -3,11 +3,16 @@ const bodyParser = require('body-parser');
 const connection = require('./pg-connection');
 const pessoaRoutes = require('./routes/pessoa.route');
 
+//Instanciar aplicação
 const app = express();
+
+//Porta da aplicação
 const port = 3000;
 
+//Adicionar o body-parser à aplicação
 app.use(bodyParser.json());
 
+//Rota para raíz
 app.get('/', (req,res) => {
     res.send('<h1>Projeto de exemplo de conexão com Banco de Dados</h1>');
 });
@@ -15,15 +20,16 @@ app.get('/', (req,res) => {
 //Adiciona rota para tabela "pessoa"
 app.use(pessoaRoutes);
 
-//Estabelece uma conexao com o banco de dados
+//Estabelece uma conexão com o banco de dados
 connection.connect()
-    //Se deu certo levanta o servico na porta 3000
+    //Se deu certo, então...
     .then(() => {
+        //Levanta o serviço na porta 3000
         app.listen(port, ()=> {
-            console.log('Rodando na porta: %s', port);
+            console.log('Executando na porta: %s', port);
         });
     })
-    //Senao
+    //...senão, deu pau!
     .catch((error) => {
-        console.log('Nao foi possivel conectar ao banco de dados: %s', error.message);
+        console.log('Não foi possível conectar ao banco de dados: %s', error.message);
     });
--- mvc-pg/routes/pessoa.route.js
+++ mvc-pg/routes/pessoa.route.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const routes = express.Router();
-const PessoaController = require('../controller/pessoa.controller')
+const PessoaController = require('../controller/pessoa.controller');
 
 //Listar todos os registros
 routes.get('/pessoas', PessoaController.find);
+
+//Adicionar um item aos registros
+routes.post('/pessoas', PessoaController.create);
+
+//Retorna apenas o item com o ID passado por parametro na URI
+routes.get('/pessoas/:id', PessoaController.findOne);
+
+//Altera o item com o ID passado por parametro na URI pelo objeto anexado
+routes.put('/pessoas/:id', PessoaController.update);
 
 module.exports = routes;
